a19ff03f96f09f69d89b12d102aff4b6ec45e46f
RahulKSaini <rahul545436@gmail.com>
2024-09-14 17:53:56 +0530

    updated route.ts

--- src/app/api/events/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-export default function GET(){
-    return  NextResponse('hello', {
-        status: 200,
-        headers: {
-            'Content-Type': 'text/plain'
-        }
-    })
-}
